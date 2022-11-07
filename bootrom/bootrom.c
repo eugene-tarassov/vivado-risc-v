d7530aa56e3a2a3da169b6c4821f96ac4bb0fac0
Eugene Tarassov <eugene@largest.net>
2022-11-06 21:46:15 -0800

    BootROM version changed to 3.5

--- bootrom/bootrom.c
+++ bootrom/bootrom.c
@@ -583,7 +583,7 @@ int main(void) {
 
     for (;;) {
         kputs("");
-        kprintf("RISC-V %d, Boot ROM V3.4\n", __riscv_xlen);
+        kprintf("RISC-V %d, Boot ROM V3.5\n", __riscv_xlen);
         drv_status = STA_NOINIT;
         errno = f_mount(&fatfs, "", 1);
         if (errno) {
